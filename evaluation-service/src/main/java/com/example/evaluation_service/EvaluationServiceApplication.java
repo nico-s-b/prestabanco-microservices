1aa7b90d8b622363bce79980ca225966d787e549
Nicolas Sepulveda <nicolas.sepulveda.b@usach.cl>
2024-11-19 05:35:12 -0300

    Initial Commit prestabanco-microservices

---
+++ evaluation-service/src/main/java/com/example/evaluation_service/EvaluationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.evaluation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EvaluationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationServiceApplication.class, args);
+	}
+
+}
