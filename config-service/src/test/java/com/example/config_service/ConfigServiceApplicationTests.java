49a79d15c876a0347ea6fdbcafa0c12eec461817
Nicolas Sepulveda <nicolas.sepulveda.b@usach.cl>
2024-11-19 23:14:14 -0300

    Config server configurations

--- config-service/src/test/java/com/example/config_service/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/example/config_service/ConfigServiceApplicationTests.java
@@ -3,7 +3,10 @@ package com.example.config_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.config.import=optional:configserver:",
+		"spring.cloud.config.enabled=false"
+})
 class ConfigServiceApplicationTests {
 
 	@Test
